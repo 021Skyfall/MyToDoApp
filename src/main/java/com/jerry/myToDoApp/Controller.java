d5bcefaad25bfc9bc2c1b31b18d89631470e9c1f
021Skyfall <021skyfall@gmail.com>
2023-04-12 14:25:18 +0900

    feat -> dev

--- src/main/java/com/jerry/myToDoApp/Controller.java
+++ src/main/java/com/jerry/myToDoApp/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/")
     public String mainPage() {
+        System.out.println("깃 브랜치 설정 feat -> dev");
         return "To-Do Application";
     }
 }
